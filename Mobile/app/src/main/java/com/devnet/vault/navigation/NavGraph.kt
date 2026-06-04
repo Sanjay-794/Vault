6d0309388a04d9ac2dca4c498bdfcc722a4e8b79
Sanjay Singh <sanjuofficial794@gmail.com>
2026-06-04 15:02:08 +0530

    feat(android): setup initial mobile project structure

---
+++ Mobile/app/src/main/java/com/devnet/vault/navigation/NavGraph.kt
@@ -0,0 +1,9 @@
+package com.devnet.vault.navigation
+
+import androidx.compose.runtime.Composable
+import com.devnet.vault.presentation.home.HomeScreen
+
+@Composable
+fun NavGraph() {
+    HomeScreen()
+}
---
+++ Mobile/app/src/main/java/com/devnet/vault/presentation/home/HomeScreen.kt
@@ -0,0 +1,9 @@
+package com.devnet.vault.presentation.home
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeScreen() {
+    Text("Vault Application")
+}
